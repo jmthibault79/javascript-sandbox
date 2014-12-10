44d280d285e5e3165bb882b8f27ab9e2296b23e4
Joel Thibault <thibault@broadinstitute.org>
2014-12-10 13:25:30 -0500

    Add toggle for link boldness

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,6 +3,7 @@ window.onload = function() {
     //alert( "alert box after rendering" );
 };
 
+var makebold = false;
 
 /* jQuery */
 $( document ).ready(function() {
@@ -10,5 +11,12 @@ $( document ).ready(function() {
 
     $( "a" ).click(function( event ) {
         //alert( "alert box on click" );
+        makebold = !makebold;
+        if (makebold) {
+            $( "a" ).addClass( "makebold" );
+        }
+        else {
+            $( "a" ).removeClass( "makebold" );
+        }
     });
 });
